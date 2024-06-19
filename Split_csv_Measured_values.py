d242998a07ea260dffc206b759fe930bccb9dcf8
piotr.troczynski <piotr.m.troczynski@gmail.com>
2024-06-20 01:05:49 +0200

    TODO MV only data copy and integration split SC, split MV, module t choose split

--- Split_csv_Measured_values.py
+++ Split_csv_Measured_values.py
@@ -10,7 +10,9 @@ a = "All.csv"
 b = "ALL_LDK_GRB_SC_30-04.04.23.csv"
 c = "SDA_ALL_BUK_MV.csv"
 d = "SDA ALL GZK WTG MV.csv"
-dir_big_file = open(b, "r")
+e = "ALL_LDK_GRB_MV_30-04.04.23.csv"
+f= "SDA_LDK01_03_MV18.06_20.06.csv"
+dir_big_file = open(f, "r")
 
 #sprawdzamy rodzaj pliku
 saved_rows = []
@@ -37,17 +39,19 @@ def Wtg_list(saved_rows):
 
 turbines_list = Wtg_list(saved_rows)
 #check end of turbines list
-print(turbines_list)
+
 
 def MeasureValues(saved_rows, turbines_list):
 
     time_to_units_block = [] #3rd block of fixed lines with additional data such as time period, creator and units
     for x in saved_rows[len(turbines_list)+3:len(turbines_list)+7]:
         time_to_units_block.append(x)
-    data_available_wtg_no = [] #block of data availability and turbines list not in order
+    data_available_wtg_no = [] #block of data availability and turbines list not in order 3* due to turbines list is iterat 2 times and one time data vailable
     for y in saved_rows[len(turbines_list)+7:len(turbines_list)*3+7]:
         data_available_wtg_no.append(y)
 
+    start_index_data = len(turbines_list) * 3 + 9
+    end_index_data = 144 #zamiast 144 to ilośc dni razy 144
 
 
     for element in turbines_list:
@@ -59,9 +63,6 @@ def MeasureValues(saved_rows, turbines_list):
         #for element in  #create loop tubine +data available not the same order as turbineslist
         for wtg in data_available_wtg_no:
              if wtg == f'{element}\n':
-                print(f'{element}\n')
-                print(data_available_wtg_no)
-                #nextfileWTG.writelines(data_available_wtg_no)
                 nextfileWTG.writelines(wtg)
                 nextfileWTG.writelines(data_available_wtg_no[
                                            data_available_wtg_no.index(wtg)+1
@@ -71,18 +72,26 @@ def MeasureValues(saved_rows, turbines_list):
         nextfileWTG.writelines(saved_rows[len(turbines_list)*3+8]) # timestamp line
         #create loop with data based on wtg number
         # time stam to ";" first line which is an index of line to start in next iteration
-        nextfileWTG.writelines(saved_rows[len(turbines_list) * 3 + 9:])
         # for dataline in saved_rows[len(turbines_list)*3+8:100]:
 
-        start_index_data = len(turbines_list) * 3 + 9
-        end_index_data = 0
-        mv_data = saved_rows[start_index_data:end_index_data]
-        for el in saved_rows[start_index_data:]:
-            if str(el)[0:1] != ";":
-                nextfileWTG.writelines(el)
-            else:
-                start_index_data = len(el)
-            break
+        nextfileWTG.writelines(saved_rows[start_index_data:end_index_data])
+
+        #mv_data = saved_rows[start_index_data:end_index_data]
+        # for el in saved_rows[start_index_data:]:
+        #     print(el[0])
+        #
+        #     if el[0] != ";":
+        #         nextfileWTG.writelines(el)
+        #
+        #     else:
+        #         start_index_data = len(el)
+        #         break
+        #     break
+        start_index_data += 144*3 #doba danych zamieć na zmienną ilość dni*144
+        start_index_data += 1
+        end_index_data += 144*3 # dlaczego ucina do 120 powinno być 144
+        end_index_data += 1
+
 
         nextfileWTG.close()
 
@@ -106,14 +115,3 @@ elif saved_rows[1] == sgre_status_code:
     StatusCode(saved_rows, turbines_list)
 else:
     print("please check if the file contain Status codes or Measured Values")
-
-
-
-new_file_MV = open("TEST_MV.csv", "w+")
-
-# for lines in saved_rows:
-#     new_file_MV.writelines(saved_rows)
-new_file_MV.writelines(saved_rows[1:33])
-
-new_file_MV.close()
-
