969400c17a714e1ded9fe7ba474242eea141e283
Troczu <p.troczynski@outlook.com>
2024-06-29 11:13:44 +0200

    Gitignore add

---
+++ upload/Split_csv_Measured_values.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+
+def time_period(saved_rows, turbines_list):
+    str_start_date = str(saved_rows[len(turbines_list) + 3])[13:23]
+    str_end_date = str(saved_rows[len(turbines_list) + 3])[26:36]
+    start_date = datetime.strptime(str_start_date, "%d/%m/%Y").date()
+    end_date = datetime.strptime(str_end_date, "%d/%m/%Y").date()
+    timedelta = (end_date - start_date).days
+    data_lines_number = 144 + 144 * timedelta
+
+    return data_lines_number
+
+
+def measure_values(saved_rows, turbines_list, number_of_lines):
+    #    below 2 variables with blocks of lines - app more efficient
+    time_to_units_block = []  # 3rd block of fixed lines with additional data such as time period, creator and units
+    for x in saved_rows[len(turbines_list) + 3:len(turbines_list) + 7]:
+        time_to_units_block.append(x)
+    data_available_wtg_no = []  # block of data availability and turbines list not in order
+    for y in saved_rows[len(turbines_list) + 7:len(turbines_list) * 3 + 7]:
+        data_available_wtg_no.append(y)
+
+    start_index_data = len(turbines_list) * 3 + 9
+    end_index_data = len(
+        turbines_list) * 3 + 9 + number_of_lines
+
+    for element in turbines_list:
+        name_of_turbine = f'MV_for_{element.replace("/", "_")}.csv'  # create name of the file based on wtg
+        nextfile_wtg = open(name_of_turbine, "w+")  # create and open file(singular wtg)
+        nextfile_wtg.writelines(saved_rows[0:3])  # save 1st fixed block of text
+        nextfile_wtg.writelines(element + '\n')
+        nextfile_wtg.writelines(time_to_units_block)
+        # for element in  #create loop turbine +data available not the same order as turbineslist
+        for wtg in data_available_wtg_no:
+            if wtg == f'{element}\n':
+                nextfile_wtg.writelines(wtg)
+                nextfile_wtg.writelines(data_available_wtg_no[
+                                           data_available_wtg_no.index(wtg) + 1
+                                           ])
+                break
+        nextfile_wtg.writelines('\n')
+        nextfile_wtg.writelines(saved_rows[len(turbines_list) * 3 + 8])  # timestamp line
+        # data copy based on index of lines in saved_rows. Every 24 hours has 144 lines of data (number of lines)
+
+        nextfile_wtg.writelines(saved_rows[start_index_data:end_index_data])
+        start_index_data += number_of_lines
+        start_index_data += 1  # lineseparator
+        end_index_data += number_of_lines
+        end_index_data += 1  # line-separator
+
+        nextfile_wtg.close()
---
+++ upload/verified_file.py
@@ -0,0 +1,36 @@
+from upload import Split_csv_Measured_values
+from upload import Split_csv_Status_Codes
+
+sgre_measured_values = '10 Minute Values, detailed\n'
+sgre_status_code = 'Status code list\n'
+
+
+def verify_file(file_path):
+    with open(file_path, 'r') as dir_big_file:
+        saved_rows1 = []
+        for row in dir_big_file:
+            saved_rows1.append(row)
+
+    def wtg_list(saved_rows):
+        turbines_list1 = []
+        start_status_codes_for = []
+        time_period = "time period:"
+        for el in saved_rows[3:]:
+            if str(el)[0:12] == time_period:
+                break
+            turbines_list1.append(el[:-1])
+            sc = [f'Status codes for  {el[:-1]}']
+            start_status_codes_for.append(sc)
+        return turbines_list1
+
+    turbines_list = wtg_list(saved_rows1)
+    data_lines_per_wtg = Split_csv_Measured_values.time_period(saved_rows1, turbines_list)
+
+    if saved_rows1[1] == sgre_measured_values:
+        Split_csv_Measured_values.measure_values(saved_rows1, turbines_list, data_lines_per_wtg)
+        return True
+    elif saved_rows1[1] == sgre_status_code:
+        Split_csv_Status_Codes.status_codes(turbines_list, file_path)
+        return True
+    else:
+        return False
